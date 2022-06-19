5aa6398bcb3240c8b30d35d6dfe9582ae5da0fe2
Alex Toland <atoland89@gmail.com>
2022-06-20 00:06:28 +1000

    init commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { ChangeEvent, useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+// import {options} from './components/Dropdown'
+
+import Slider from "@mui/material/Slider";
+
+import Select from "react-select"
+
+const options = [
+  { label: "Sine", value: 1 },
+  { label: "Square", value: 2 },
+  { label: "Sawtooth", value: 3 },
+  { label: "Triangle", value: 4 },
+  { label: "Random", value: 5 },
+];
 
 function App() {
+
+  const [rate, setRate] = useState('');
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <body>
+    <h1>Waves</h1>
+    <div className="wave1">
+      <h2>Wave 1</h2>
+      <Select options={options} />
+      <Slider aria-label="Rate" value={rate} onChange={e => setRate<HTMLInputElement>(event.target.value)} />
+      rate
+      amp
+      phase
     </div>
+    
+    <div className="wave2">
+      <h2>Wave 2</h2>
+      <Select options={options} />
+      rate
+      amp
+      phase
+    </div>
+
+    <div className='wave3'>
+      <h2>Wave Combo</h2>
+    </div>
+  </body>
   );
 }
 
 export default App;
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//        <div className="wave1">Wave 1</div>
+//        <div>
+//        </div>
+//        <div className="wave2">Wave 2</div>
+//        <div className="waveCombo">Wave Combo</div> 
+//       </header>
+//     </div>
+//   );
+// }
+
---
+++ src/components/Dropdown.tsx
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+
+export const options = [
+  {
+    id: 0,
+    title: 'Sine',
+    selected: false,
+    key: 'name'
+  },
+  {
+    id: 1,
+    title: 'Square',
+    selected: false,
+    key: 'name'
+  },
+  {
+    id: 2,
+    title: 'Sawtooth',
+    selected: false,
+    key: 'name'
+  },
+  {
+    id: 3,
+    title: 'Triangle',
+    selected: false,
+    key: 'name'
+  },
+  {
+    id: 4,
+    title: 'Random',
+    selected: false,
+    key: 'name'
+  }
+]
+
